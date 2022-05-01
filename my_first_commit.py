afe234c33fadf8cbdf2c91060f917e344729ed0b
Stalomir <slawomir.kapinos@gmail.com>
2022-05-01 11:16:34 +0200

    Zabawa kolorami

--- my_first_commit.py
+++ my_first_commit.py
@@ -4,6 +4,6 @@ item_number = 0
 for k in shopping_list:
     shopping_list[k] = [i.capitalize() for i in shopping_list[k]]
     item_number = item_number + len(shopping_list[k])
-[print('Idę do',k.title(),'i kupuję tam',v) for k, v in shopping_list.items()]
-print('W sumie kupuję',item_number,'produktów')
+[print("\033[1;37mIdę do",k.title(),"i kupuję tam",v) for k, v in shopping_list.items()]
+print('\033[1;31mW sumie kupuję',item_number,'produktów')
     
